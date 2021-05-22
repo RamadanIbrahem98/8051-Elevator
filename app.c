fe269c3e635db0d70ac38e41b7037145c9febee5
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2021-05-22 16:13:48 +0200

    add Makefile and app boilerplate

---
+++ app.c
@@ -0,0 +1,17 @@
+#include "lib/Main.h"
+#include "lib/Port.h"
+#include "lib/Common.h"
+
+void main()
+{
+    // diable the watch dog
+    WDTCN = 0x0DE;
+    WDTCN = 0x0AD;
+
+    OSCICN = 0x014; // 2MH clock
+
+    // config cross bar
+    XBR0 = 0x00;
+    XBR1 = 0x00;
+    XBR2 = 0x040; // Cross bar enabled , weak Pull-up enabled
+}
